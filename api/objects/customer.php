1aa59cfccb708a82b69936d64019f8d95c878238
Joab <joab@Joabs-MacBook-Pro.local>
2019-04-05 02:33:02 -0300

    first commit bullet

---
+++ api/objects/customer.php
@@ -0,0 +1,101 @@
+<?php
+class Customer{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "customers";
+ 
+    // object properties
+    public $id;
+    public $name;
+    public $type;
+    public $email;
+    public $phone;
+    public $status;
+    public $created;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+
+    // read customers
+    function read($id){
+        if($id == 0){
+            // select all query
+            $query = "SELECT
+                c.id, c.name, c.email, c.phone, c.created, 
+                case
+                    when c.type = 1 then 'Pessoa Física' else 'Pessoa Jurídica'
+                end type,
+                case
+                    when c.status = 1 then 'Ativo' else 'Inativo' 
+                end status
+                FROM
+                " . $this->table_name . " c
+                ORDER BY
+                c.created DESC";
+        }else{
+            // select all query
+            $query = "SELECT
+                c.id, c.name, c.email, c.phone, c.created, 
+                case
+                    when c.type = 1 then 'Pessoa Física' else 'Pessoa Jurídica'
+                end type,
+                case
+                    when c.status = 1 then 'Ativo' else 'Inativo' 
+                end status
+                FROM
+                " . $this->table_name . " c
+                WHERE 
+                    id = " . $id . " 
+                ORDER BY
+                c.created DESC";
+        }
+        
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // execute query
+        $stmt->execute();
+    
+        return $stmt;
+    }
+
+    // create customer
+    function create(){
+    
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    name=:name, type=:type, email=:email, phone=:phone, status=:status, created=:created";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->type=htmlspecialchars(strip_tags($this->type));
+        $this->email=htmlspecialchars(strip_tags($this->email));
+        $this->phone=htmlspecialchars(strip_tags($this->phone));
+        $this->status=htmlspecialchars(strip_tags($this->status));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+    
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":type", $this->type);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":phone", $this->phone);
+        $stmt->bindParam(":status", $this->status);
+        $stmt->bindParam(":created", $this->created);
+        
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+        
+        return false;
+    }
+}
